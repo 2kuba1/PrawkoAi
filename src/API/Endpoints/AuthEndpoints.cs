dc5896ace38e50d2969cfe3fa7b2ca1babc43b80
Jakub Wojtyna <jakub.wojtyna06@gmail.com>
2026-03-09 20:03:54 +0100

    Add support for native login

--- src/API/Endpoints/AuthEndpoints.cs
+++ src/API/Endpoints/AuthEndpoints.cs
@@ -1,12 +1,14 @@
 ﻿using Application.Features.Users.GoogleLogin;
+using Application.Features.Users.GoogleLoginNative;
 using Application.Features.Users.GuestLogin;
 using Application.Features.Users.RefreshAuthToken;
 using Application.Features.Users.RevokeRefreshTokens;
-using Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Mvc;
+using Google.Apis;
+using Google.Apis.Auth;
 
 namespace API.Endpoints;
 
@@ -20,21 +22,42 @@ public static class AuthEndpoints
         app.MapGet("/api/account/refresh-token", RefreshToken);
         app.MapDelete("/api/account/logout", Logout)
             .RequireAuthorization();
-        
+        app.MapPost("/api/account/login/google-native", GoogleLoginNative);
+    }
+
+    private static async Task<IResult> GoogleLoginNative([FromBody] GoogleLoginRequest request, [FromServices] IMediator mediator, [FromServices] IConfiguration configuration)
+    {
+        try
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = [configuration.GetValue<string>("Google:GoogleNativeAuthSecret")],
+            };
+
+            var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken, settings);
+            
+            var tokens = await mediator.Send(new GoogleLoginNative(payload.Email));
+
+            return Results.Ok(tokens);
+        }
+        catch (InvalidJwtException)
+        {
+            return Results.Unauthorized();
+        }
     }
 
     private static IResult GoogleLogin(
-        [FromQuery]string? returnUrl,
+        [FromQuery] string? returnUrl,
         HttpContext httpContext)
     {
         return Results.Challenge(new AuthenticationProperties()
         {
             RedirectUri = "/api/account/login/google/callback",
-        },["Google"]);
+        }, ["Google"]);
     }
 
     private static async Task<IResult> GoogleLoginCallback(HttpContext httpContext,
-        [FromServices]IMediator mediator)
+        [FromServices] IMediator mediator)
     {
         var result = await httpContext.AuthenticateAsync(GoogleDefaults.AuthenticationScheme);
 
@@ -42,25 +65,28 @@ public static class AuthEndpoints
             return Results.Unauthorized();
 
         var tokens = await mediator.Send(new GoogleLogin(result.Principal), CancellationToken.None);
-        
+
         return Results.Ok(tokens);
     }
 
-    private static async Task<IResult> Logout([FromQuery]Guid userId, [FromServices] IMediator mediator)
+    private static async Task<IResult> Logout([FromQuery] Guid userId, [FromServices] IMediator mediator)
     {
         var result = await mediator.Send(new RevokeRefreshTokens(userId));
         return Results.Ok(result);
     }
 
-    private static async Task<IResult> GuestLogin([FromQuery]string deviceId, [FromServices] IMediator mediator)
+    private static async Task<IResult> GuestLogin([FromQuery] string deviceId, [FromServices] IMediator mediator)
     {
         var tokens = await mediator.Send(new GuestLogin(deviceId));
         return Results.Ok(tokens);
     }
 
-    private static async Task<IResult> RefreshToken([FromQuery]string refreshToken, [FromServices] IMediator mediator)
+    private static async Task<IResult> RefreshToken([FromQuery] string refreshToken, [FromServices] IMediator mediator)
     {
         var newTokens = await mediator.Send(new RefreshAuthToken(refreshToken));
         return Results.Ok(newTokens);
     }
+
+    private record GoogleLoginRequest(string IdToken);
 }
+ 
---
+++ src/Application/Features/Users/GoogleLoginNative/GoogleLoginNativeHandler.cs
@@ -0,0 +1,65 @@
+﻿using Application.Contracts.Repositories;
+using Application.Contracts.Services;
+using Application.Models;
+using Domain.Entities;
+using Domain.Exceptions;
+using MediatR;
+
+namespace Application.Features.Users.GoogleLoginNative;
+
+internal sealed class GoogleLoginNativeHandler : IRequestHandler<GoogleLoginNative, TokenResponse>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IRoleRepository _roleRepository;
+    private readonly IAuthService _authService;
+    private readonly IRefreshTokenRepository _refreshTokenRepository;
+
+    public GoogleLoginNativeHandler(IUserRepository userRepository, IRoleRepository roleRepository, IAuthService authService, IRefreshTokenRepository refreshTokenRepository)
+    {
+        _userRepository = userRepository;
+        _roleRepository = roleRepository;
+        _authService = authService;
+        _refreshTokenRepository = refreshTokenRepository;
+    }
+    
+    public async Task<TokenResponse> Handle(GoogleLoginNative request, CancellationToken cancellationToken)
+    {
+        if(request.Email is null)
+            throw new NullParametersException("Claims cannot be null");
+
+        var user = await _userRepository.FindUserByEmailAsync(request.Email);
+
+        if (user is null)
+        {
+            var role = await _roleRepository.GetRoleByName("User");
+            
+            if(role is null)
+                throw new NotFoundException($"Role does not exist");
+            
+            var newUser = new User()
+            {
+                Email = request.Email,
+                DeviceId = "123",
+                RoleId =  role.Id,
+            };
+
+            await _userRepository.CreateAsync(newUser);
+            
+            user = newUser;
+        }
+
+        var token = _authService.CreateToken(user);
+
+        var refreshToken = new RefreshToken()
+        {
+            Id = Guid.NewGuid(),
+            UserId = user.Id,
+            Token = _authService.GenerateRefreshToken(),
+            ExpiresOnUtc = DateTime.UtcNow.AddDays(30)
+        };
+        
+        await _refreshTokenRepository.CreateAsync(refreshToken);
+        
+        return new TokenResponse(token, refreshToken.Token);
+    }
+}
