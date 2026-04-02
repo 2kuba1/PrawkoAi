92aefbee4abdf720dab597fe076a64be68ef90d6
Jakub Wojtyna <jakub.wojtyna06@gmail.com>
2026-04-02 22:35:34 +0200

    Remove questions with translations to json, change prompt in question ai analyzer

--- Tools/XlsxReader/main.py
+++ Tools/XlsxReader/main.py
@@ -1,11 +1,19 @@
-#pip install pandas openpyxl
+# pip install pandas openpyxl
 import pandas as pd
 
-xlspath = r""
+xlspath = r"C:\Users\jakub\Desktop\katalog_dla_kandydatów_na_kierowców_0220266.xlsx"
 
-xls = pd.read_excel(xlspath, usecols=['Numer pytania', 'Pytanie', 'Odpowiedź A', 'Odpowiedź B', 'Odpowiedź C', 'Poprawna odp', 'Media', 'Zakres struktury', 'Liczba punktów', 'Kategorie'])
+xls = pd.read_excel(xlspath, usecols=[
+    'Numer pytania', 'Pytanie', 'Odpowiedź A', 'Odpowiedź B', 'Odpowiedź C', 
+    'Poprawna odp', 'Media', 'Zakres struktury', 'Liczba punktów', 'Kategorie', 
+    'Pytanie [EN]', 'Odpowiedź A [EN]', 'Odpowiedź B [EN]', 'Odpowiedź C [EN]', 
+    'Pytanie [D]', 'Odpowiedź A [D]', 'Odpowiedź B [D]', 'Odpowiedź C [D]', 
+    'Pytanie [UA]', 'Odpowiedź A [UA]', 'Odpowiedź B [UA]', 'Odpowiedź C [UA]'
+])
+
 print(xls.head())
 
 with open("questions.txt", "a", encoding="utf-8") as f:
     for _, row in xls.iterrows():
-        f.write("@ ".join(str(item) for item in row) + "\n")
+        processed_row = [str(item).strip() for item in row]
+        f.write("@".join(processed_row) + "\n")
